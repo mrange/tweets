fcac20438e03a43da1e3baeb1f4aecab63ffd2ce
mrange <marten_range@hotmail.com>
2013-05-22 21:35:18 +0200

    1. Added Exception generator sample

---
+++ tweets/GeneratingExceptionClasses/Generated_Exceptions.cs
@@ -0,0 +1,277 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+
+
+// ReSharper disable InvocationIsSkipped
+// ReSharper disable PartialMethodWithSinglePart
+// ReSharper disable PartialTypeWithSinglePart
+
+namespace GeneratingExceptionClasses
+{
+    using System;
+    using System.Runtime.Serialization;
+    using System.Security.Permissions;
+
+ 
+
+    /// <summary>
+    /// Represents AuthorizationRequired errors that occur during application execution.
+     /// </summary>
+    [Serializable]
+    public partial class AuthorizationRequiredException : Exception
+    {
+        partial void Construct_AuthorizationRequiredException();
+        partial void Serialize_AuthorizationRequiredException(SerializationInfo info, StreamingContext context);
+        partial void Unserialize_AuthorizationRequiredException(SerializationInfo info);
+
+        public AuthorizationRequiredException()
+        {
+            Construct_AuthorizationRequiredException();
+        }
+ 
+        public AuthorizationRequiredException(string message)
+            : base(message)
+        {
+            Construct_AuthorizationRequiredException();
+        }
+ 
+        public AuthorizationRequiredException(string message, Exception inner)
+            : base(message, inner)
+        {
+            Construct_AuthorizationRequiredException();
+        }
+ 
+        protected AuthorizationRequiredException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            Unserialize_AuthorizationRequiredException(info);
+            Construct_AuthorizationRequiredException();
+        }
+ 
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            Serialize_AuthorizationRequiredException(info, context);
+
+            base.GetObjectData(info, context);
+        }
+ 
+    }
+
+
+    /// <summary>
+    /// Represents BasicHttpAuthorizationRequired errors that occur during application execution.
+     /// </summary>
+    [Serializable]
+    public partial class BasicHttpAuthorizationRequiredException : AuthorizationRequiredException
+    {
+        partial void Construct_BasicHttpAuthorizationRequiredException();
+        partial void Serialize_BasicHttpAuthorizationRequiredException(SerializationInfo info, StreamingContext context);
+        partial void Unserialize_BasicHttpAuthorizationRequiredException(SerializationInfo info);
+
+        public BasicHttpAuthorizationRequiredException()
+        {
+            Construct_BasicHttpAuthorizationRequiredException();
+        }
+ 
+        public BasicHttpAuthorizationRequiredException(string message)
+            : base(message)
+        {
+            Construct_BasicHttpAuthorizationRequiredException();
+        }
+ 
+        public BasicHttpAuthorizationRequiredException(string message, Exception inner)
+            : base(message, inner)
+        {
+            Construct_BasicHttpAuthorizationRequiredException();
+        }
+ 
+        protected BasicHttpAuthorizationRequiredException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            Unserialize_BasicHttpAuthorizationRequiredException(info);
+            Construct_BasicHttpAuthorizationRequiredException();
+        }
+ 
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            Serialize_BasicHttpAuthorizationRequiredException(info, context);
+
+            base.GetObjectData(info, context);
+        }
+ 
+    }
+
+
+    /// <summary>
+    /// Represents WindowsAuthorizationRequired errors that occur during application execution.
+     /// </summary>
+    [Serializable]
+    public partial class WindowsAuthorizationRequiredException : AuthorizationRequiredException
+    {
+        partial void Construct_WindowsAuthorizationRequiredException();
+        partial void Serialize_WindowsAuthorizationRequiredException(SerializationInfo info, StreamingContext context);
+        partial void Unserialize_WindowsAuthorizationRequiredException(SerializationInfo info);
+
+        public WindowsAuthorizationRequiredException()
+        {
+            Construct_WindowsAuthorizationRequiredException();
+        }
+ 
+        public WindowsAuthorizationRequiredException(string message)
+            : base(message)
+        {
+            Construct_WindowsAuthorizationRequiredException();
+        }
+ 
+        public WindowsAuthorizationRequiredException(string message, Exception inner)
+            : base(message, inner)
+        {
+            Construct_WindowsAuthorizationRequiredException();
+        }
+ 
+        protected WindowsAuthorizationRequiredException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            Unserialize_WindowsAuthorizationRequiredException(info);
+            Construct_WindowsAuthorizationRequiredException();
+        }
+ 
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            Serialize_WindowsAuthorizationRequiredException(info, context);
+
+            base.GetObjectData(info, context);
+        }
+ 
+    }
+
+
+    /// <summary>
+    /// Represents FormsAuthorizationRequired errors that occur during application execution.
+     /// </summary>
+    [Serializable]
+    public partial class FormsAuthorizationRequiredException : AuthorizationRequiredException
+    {
+        partial void Construct_FormsAuthorizationRequiredException();
+        partial void Serialize_FormsAuthorizationRequiredException(SerializationInfo info, StreamingContext context);
+        partial void Unserialize_FormsAuthorizationRequiredException(SerializationInfo info);
+
+        public FormsAuthorizationRequiredException()
+        {
+            Construct_FormsAuthorizationRequiredException();
+        }
+ 
+        public FormsAuthorizationRequiredException(string message)
+            : base(message)
+        {
+            Construct_FormsAuthorizationRequiredException();
+        }
+ 
+        public FormsAuthorizationRequiredException(string message, Exception inner)
+            : base(message, inner)
+        {
+            Construct_FormsAuthorizationRequiredException();
+        }
+ 
+        protected FormsAuthorizationRequiredException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            Unserialize_FormsAuthorizationRequiredException(info);
+            Construct_FormsAuthorizationRequiredException();
+        }
+ 
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            Serialize_FormsAuthorizationRequiredException(info, context);
+
+            base.GetObjectData(info, context);
+        }
+ 
+    }
+
+
+    /// <summary>
+    /// Represents ClaimsAuthorizationRequired errors that occur during application execution.
+     /// </summary>
+    [Serializable]
+    public partial class ClaimsAuthorizationRequiredException : AuthorizationRequiredException
+    {
+        partial void Construct_ClaimsAuthorizationRequiredException();
+        partial void Serialize_ClaimsAuthorizationRequiredException(SerializationInfo info, StreamingContext context);
+        partial void Unserialize_ClaimsAuthorizationRequiredException(SerializationInfo info);
+
+        public ClaimsAuthorizationRequiredException()
+        {
+            Construct_ClaimsAuthorizationRequiredException();
+        }
+ 
+        public ClaimsAuthorizationRequiredException(string message)
+            : base(message)
+        {
+            Construct_ClaimsAuthorizationRequiredException();
+        }
+ 
+        public ClaimsAuthorizationRequiredException(string message, Exception inner)
+            : base(message, inner)
+        {
+            Construct_ClaimsAuthorizationRequiredException();
+        }
+ 
+        protected ClaimsAuthorizationRequiredException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            Unserialize_ClaimsAuthorizationRequiredException(info);
+            Construct_ClaimsAuthorizationRequiredException();
+        }
+ 
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            Serialize_ClaimsAuthorizationRequiredException(info, context);
+
+            base.GetObjectData(info, context);
+        }
+ 
+    }
+
+}
+
---
+++ tweets/GeneratingExceptionClasses/Program.cs
@@ -0,0 +1,70 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+using System;
+
+namespace GeneratingExceptionClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine (@"
+Hi there!
+
+The purpose of this program is to show how to generate proper exceptions using 
+T4.
+
+This is a response to the excellent blog post: 
+http://blogs.msdn.com/b/agileer/archive/2013/05/17/the-right-way-to-write-a-custom-exception-class.aspx
+
+
+The blog post told us how an exception needs to look but didn't tell us how 
+to avoid maintaining code pattern duplication that is intrinsic of exceptions 
+classes 
+(because C# lacks constructor forwarding).
+
+Code-snippets is not the answer as they are the dark-side equivalent of 
+programming.
+
+Stay on the light-side and use a MetaProgramming tool like T4 to generate 
+exceptions.
+
+T4 is shipped in VS2008+ and the crucial difference between MetaProgramming and
+snippets is that you don't lose the connection between the model and the 
+generated code.
+
+This is essential in order to refactor the generated code.
+
+The T4 template is split into two parts.
+
+Model   - Generated_Exceptions.tt  -    Contains *what* exceptions we like 
+                                        to have generated.
+Template- Exceptions.ttinclude     -    Reusuable exception generator 
+                                        template, can be shared between 
+                                        projects
+
+In addition the example demonstrate how to use partial to extend generated 
+exceptions with custom properties.
+
+Have fun @marten_range (twitter)
+");
+
+            // Sample on how to use the generated exception.
+            // Just like you normally would
+            //throw new BasicHttpAuthorizationRequiredException
+            //    {
+            //        ResourceReferenceProperty = "This is the resource reference"        ,
+            //    };
+        }
+    }
+}
